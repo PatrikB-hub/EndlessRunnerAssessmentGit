94fdd2aaf4c7b1a5e449758157bd9841d661a9e9
PatrikB-hub <pattty306@gmail.com>
2020-08-17 21:29:57 +1000

    beginning of program. ice move left, fox stay in position.

---
+++ Fox Run - Endless Runner/Assets/Scripts/FoxConstantPosition.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+//[RequireComponent(typeof(Rigidbody2D))]
+public class FoxConstantPosition : MonoBehaviour
+{
+    public int foxgeneralposition = -4;
+
+    public float foxreturnspeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //when fox x-position < -4f move right approaching x=4
+        if (transform.position.x < foxgeneralposition)
+        {
+            Vector2 playerposition = transform.position;
+            playerposition.x += foxreturnspeed * Time.deltaTime;
+            transform.position = playerposition;
+        }
+
+        //when fox x-position > -4f move left approaching x=4
+        if (transform.position.x > foxgeneralposition)
+        {
+            Vector2 playerposition = transform.position;
+            playerposition.x -= foxreturnspeed * Time.deltaTime;
+            transform.position = playerposition;
+        }
+    }
+}
---
+++ Fox Run - Endless Runner/Assets/Scripts/GroundMoveLeft.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundMoveLeft : MonoBehaviour
+{
+    //GameObject ground;
+    public float groundspeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ground = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 groundPosition = transform.position;
+        groundPosition.x -= groundspeed * Time.deltaTime;
+        transform.position = groundPosition;
+    }
+}
