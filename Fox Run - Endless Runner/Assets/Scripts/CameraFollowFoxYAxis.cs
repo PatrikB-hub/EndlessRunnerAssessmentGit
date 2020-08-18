656689639ccf29a941991627bc3f1c3b48cc6dac
PatrikB-hub <pattty306@gmail.com>
2020-08-18 21:47:56 +1000

    start of camera follow player in bounds

---
+++ Fox Run - Endless Runner/Assets/Scripts/CameraFollowFoxYAxis.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowFoxYAxis : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject camera;
+
+    public float bottomboundary = -2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //finding player position
+        //Vector2 playerposition = player.transform.position;
+        //Debug.Log(player.transform.position.y);
+
+        //if player pos is less than (bottomboundary), camera y = player y value
+        if (player.transform.position.y < bottomboundary)
+        {
+            //replace y value of camera with player y value
+
+            Vector2 cameraposition = camera.transform.position;
+            cameraposition.y = player.transform.position.y;
+            camera.transform.position = cameraposition;
+
+        }
+    }
+}
